65d877b6b47440ad07ea91fec208cb07e5a68f94
marwan osama <mo989989@gmail.com>
2021-12-23 08:56:08 +0200

    some edits to the products grid

--- js/app.js
+++ js/app.js
@@ -5,17 +5,5 @@ const closeModalBox = document.getElementById("application-modal-box-close");
 const handleToggleModal = function() {
     $(modalBox).fadeToggle();
 }
-const calcResize = function() {
-
-    const productsFlxBx = document.getElementById("products");
-    const productsFlxBxMaxWidth = productsFlxBx.parentElement.offsetWidth;
-    const productCard = document.getElementsByClassName("product-card-wrapper")[0];
-    const productCardWidth = productCard.getBoundingClientRect().width + (parseInt(window.getComputedStyle(productCard).getPropertyValue("margin-left").match(/(\d+)/))*2);
-    if (Math.floor(productsFlxBxMaxWidth / productCardWidth) * productCardWidth !== 0) {
-        productsFlxBx.setAttribute("style",`width:${Math.floor(productsFlxBxMaxWidth / productCardWidth) * productCardWidth}px`);
-    }
-}
-window.onresize = calcResize;
-calcResize();
 showModalBox.addEventListener("click", handleToggleModal);
 closeModalBox.addEventListener("click", handleToggleModal);
